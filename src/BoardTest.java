ba31dd59ccce12339668eade1bc819e481ef5c66
Andrew Ho <andrewyc.ho@gmail.com>
2015-02-27 21:40:19 +0000

    wrote code for StateTest and BoardTest

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -8,10 +8,8 @@ public class BoardTest {
     public void setUp() throws Exception {
         testBoard = new Board();
         for(int i=0;i<6;i++) {
-            testBoard.makeMove(new Move((i % 2 != 0 ? Player.YELLOW : Player.RED), 3));
+            testBoard.makeMove(new Move((i % 2 != 0 ? Player.YELLOW : Player.RED), 0));
         }
-
-
     }
 
     @org.junit.After
@@ -22,12 +20,12 @@ public class BoardTest {
     @org.junit.Test
     public void testMakeMove() throws Exception {
         for(int i = 0;i<6;i++){
-            assert(testBoard.getTile(5-i,3) == (i%2!=0 ? Player.YELLOW : Player.RED));
+            assert(testBoard.getTile(5-i,0) == (i%2!=0 ? Player.YELLOW : Player.RED));
         }
     }
     @Test(expected = IllegalArgumentException.class)
     public void testMakeMoveThrowsFullColumnException(){
-        testBoard.makeMove(new Move(Player.RED,3));
+        testBoard.makeMove(new Move(Player.RED,0));
     }
 
     @Test (expected = IllegalArgumentException.class)
@@ -42,6 +40,47 @@ public class BoardTest {
 
     @org.junit.Test
     public void testGetPossibleMoves() throws Exception {
-        
+        Move[] testMoves = testBoard.getPossibleMoves(Player.RED);
+
+        for(int i = 0;i<5;i++){
+            assert(testMoves[i].getPlayer() == Player.RED);
+            assert(testMoves[i].getColumn() == i+1);
+            assert(testMoves.length == 6);
+        }
+        for(int j=0;j<4;j++) testBoard.makeMove(new Move(Player.YELLOW,1));
+        Move[] output = testBoard.getPossibleMoves(Player.RED);
+        Move[] expected = {};
+        assertArrayEquals(expected,output);
+    }
+
+    @Test
+    public void testGetPossibleMovesCompletelyFilledBoard(){
+
+        for(int i = 1;i<7;i++){
+            for(int j=0;j<6;j++){
+                testBoard.makeMove(new Move(fillBoardHelper(i,j),i));
+            }
+        }
+
+        Move[] output = testBoard.getPossibleMoves(Player.RED);
+        Move[] expected = {};
+        assertArrayEquals(expected,output);
+
+    }
+
+    // helper function to fill the board to create a tie game
+    private Player fillBoardHelper(int i, int j){
+        if(j%2==0)
+        { if(i%3==0){
+            return Player.RED;
+        }else{
+            return Player.YELLOW;
+        }}else{
+            if(i%3==0){
+                return Player.YELLOW;
+            }else{
+                return Player.RED;
+            }
+        }
     }
 }
---
+++ src/StateTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StateTest {
+    Board testBoard;
+    State testState;
+    Board dupBoard;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        testBoard = new Board();
+        testState = new State(Player.RED, testBoard, null);
+        for (int i = 0; i < 6; i++) {
+            testBoard.makeMove(new Move((i % 2 != 0 ? Player.YELLOW : Player.RED), 0));
+        }
+        dupBoard = testBoard;
+    }
+
+    @Test
+    public void testInitializeChildren() throws Exception {
+        testState.initializeChildren();
+        int count = 0;
+        for (State child : testState.getChildren()) {
+            assert (child.getLastMove().getPlayer() == Player.RED);
+            assertEquals(child.getLastMove().getColumn(), count + 1);
+            for (int j = 0; j < 6; j++) {
+                assert (child.getBoard().getTile(6 - j, 0) == (j % 2 != 0 ? Player.YELLOW : Player.RED));
+                assertNull (child.getChildren());
+            }
+            count++;
+        }
+        assertEquals(6, count);
+    }
+}
